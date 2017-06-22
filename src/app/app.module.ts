10233052901a39bf1041f3d2d16e370862a898c8
Niko Lagman <Lagman.Niko@gmail.com>
2017-06-22 01:31:27 -0400

    Add music to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { MenuPage } from '../pages/menu/menu';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+		SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 /**
  * Generated class for the MenuPage page.
  *
@@ -14,11 +13,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	constructor(public navCtrl: NavController, public navParams: NavParams ) {
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
-  }
-
+	}
+	ionViewDidEnter () {
+					//		document.getElementById("menuBg").play();
+	}
+	ionViewWillLeave () {
+	}
+	ionViewWillUnload () {
+	}
 }
