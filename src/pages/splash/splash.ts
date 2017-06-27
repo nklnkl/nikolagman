3700aa64360ba2f87cbd8a7d39ce70f5966ab301
Niko Lagman <Lagman.Niko@gmail.com>
2017-06-27 15:51:57 -0400

    Add sound to splash

--- src/pages/splash/splash.ts
+++ src/pages/splash/splash.ts
@@ -16,6 +16,9 @@ import { MenuPage } from '../menu/menu';
 export class SplashPage {
 	private menuPage = MenuPage;
   private audio: HTMLAudioElement;
+  private exitAudio: HTMLAudioElement;
+  private hoverAudio: HTMLAudioElement;
+  private leaving: boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -23,6 +26,10 @@ export class SplashPage {
     console.log('ionViewDidLoad SplashPage');
 		this.audio = new Audio('http://163.238.35.166/~lagman/media/splash.mp3');
 		this.audio.loop = true;
+    this.exitAudio = new Audio('http://163.238.35.166/~lagman/media/splashExit.wav');
+    this.exitAudio.loop = false;
+    this.hoverAudio = new Audio('http://163.238.35.166/~lagman/media/splashHover.wav');
+    this.hoverAudio.loop = false;
   }
   ionViewDidEnter () {
     this.audio.play();
@@ -30,5 +37,19 @@ export class SplashPage {
 	ionViewWillLeave () {
     this.audio.pause();
 	}
-
+  private hoverButton () : void {
+    this.hoverAudio.play();
+  }
+	private enterMenu () : void {
+    this.leaving = true;
+    this.exitAudio.play();
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve();
+			}, 1000);
+		})
+		.then(() => {
+			this.navCtrl.push(MenuPage);
+		});
+	}
 }
