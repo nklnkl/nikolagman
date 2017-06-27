0ec893ace232479160a80123ae92c72599e78d19
Niko Lagman <Lagman.Niko@gmail.com>
2017-06-27 17:21:39 -0400

    Add sound and fade to menu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -14,6 +14,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MenuPage {
 
   private audio: HTMLAudioElement;
+  private hoverAudio: HTMLAudioElement;
+  private selectAudio: HTMLAudioElement;
+  private leaving: boolean = false;
 
 	constructor(public navCtrl: NavController, public navParams: NavParams ) {
   }
@@ -22,11 +25,33 @@ export class MenuPage {
     console.log('ionViewDidLoad MenuPage');
 		this.audio = new Audio('http://163.238.35.166/~lagman/media/menu.mp3');
 		this.audio.loop = true;
+
+    this.hoverAudio = new Audio('http://163.238.35.166/~lagman/media/menu-hover.wav');
+		this.hoverAudio.loop = false;
+
+    this.selectAudio = new Audio('http://163.238.35.166/~lagman/media/menu-select.wav');
+		this.selectAudio.loop = false;
   }
   ionViewDidEnter () {
+    this.leaving = false;
     this.audio.play();
   }
 	ionViewWillLeave () {
     this.audio.pause();
 	}
+  private playHoverSound () : void {
+    this.hoverAudio.play();
+  }
+  private openProjects() : void {
+    this.selectAudio.play();
+    this.leaving = true;
+  }
+  private openAbout () : void {
+    this.selectAudio.play();
+    this.leaving = true;
+  }
+  private openContact () : void {
+    this.selectAudio.play();
+    this.leaving = true;
+  }
 }
