474a2bfe68bccc446abbf05967cc0741f75cca9f
Niko Lagman <Lagman.Niko@gmail.com>
2017-06-29 18:53:09 -0400

    Improve projects page

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Alert, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ProjectsPage page.
@@ -14,11 +14,64 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProjectsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private backgroundAudio: HTMLAudioElement;
+  private confirmAudio: HTMLAudioElement;
+
+  private preview: number;
+
+  private alert: Alert;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProjectsPage');
+    this.backgroundAudio = new Audio('http://163.238.35.166/~lagman/media/nikolagman/projects/background.mp3');
+    this.backgroundAudio.loop = true;
+    this.confirmAudio = new Audio('http://163.238.35.166/~lagman/media/nikolagman/projects/mario_ok.wav');
+    this.confirmAudio.loop = false;
+    return;
+  }
+  ionViewDidEnter () : void {
+    this.backgroundAudio.play();
+    this.preview = 1;
+    return;
+  }
+
+  private changePreview(i: number) : void {
+    this.preview = i;
+    return;
+  }
+
+  private openProject (i: number) : void {
+    this.confirmAudio.play();
+    switch (i) {
+      case 1:
+        this.alertCtrl.create({
+          title: 'Wait!',
+          subTitle: 'You are already on it. This website is already project #1.',
+          buttons: ['Oh, okay!']
+        }).present();
+        break;
+      case 2:
+        window.open('http://163.238.35.166/~lagman/reverse/');
+      case 3:
+        this.alertCtrl.create({
+          title: 'Not yet available!',
+          subTitle: 'This level has not been unlocked yet.',
+          buttons: ['Oh, darn it!']
+        }).present();
+        break;
+      case 4:
+        this.alertCtrl.create({
+          title: 'Not yet available!',
+          subTitle: 'This level has not been unlocked yet.',
+          buttons: ['Oh, darn it!']
+        }).present();
+        break;
+      default:
+    }
   }
 
 }
